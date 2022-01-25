d6f2f84ce9a9a75e080c0fff6147ddc1597f2db5
Jake Hracho <jhracho@nd.edu>
2022-01-25 16:22:26 -0500

    reorganized

---
+++ src/Components/Components.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+import BoardPage from './BoardPage/BoardPage.js';
+
+const Components = () =>{
+    return(
+        <BoardPage/>
+    );
+}
+
+export default Components;
