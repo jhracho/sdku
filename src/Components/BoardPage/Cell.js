d39326ab5ccce3f0ef2451b33e7a4bbf4660b4e3
Jake Hracho <jhracho@nd.edu>
2022-01-26 15:06:52 -0500

    Board fills out based on puzzle matrix, started prefill skips

--- src/Components/BoardPage/Cell.js
+++ src/Components/BoardPage/Cell.js
@@ -2,12 +2,21 @@ import React, {Fragment} from "react";
 import { NumFill } from "../../Services/NumFill.js";
 
 const Cell = (id, char) =>{
-    var i = 'c' + (id.id + 1);
-    return(
-        <Fragment>
-            <input id = {i}  className="cell" type="text"  onKeyUp={NumFill} minLength="1" maxLength="1"/>
-        </Fragment>
-    )
+    const num = id.char;
+    if (num != '-'){
+        return(
+            <Fragment>
+                <input id = {'c' + (id.id + 1)} placeholder={num} readOnly='readonly' className="cell prefill" type="text" onKeyUp={NumFill}/>
+            </Fragment> 
+        )
+    }
+    else{
+        return(
+            <Fragment>
+                <input id = {'c' + (id.id + 1)} className="cell" type="text"  onKeyUp={NumFill} minLength="1" maxLength="1"/>
+            </Fragment>
+        )
+    }
 };
 
 export default Cell;
--- src/Services/NumFill.js
+++ src/Services/NumFill.js
@@ -21,28 +21,49 @@ export const NumFill = (event) =>{
         // Left arrow
         if (event.which === 37){
             var num = start - 1;
+            // Top Left to Bottom Right Wrapping
+            if (num === 0){ num = 81; }
+            // Skipping prefills
+            while (document.getElementById('c'+ num).classList.contains('prefill')){
+                num -= 1;
+                if (num === 0) { num = 81; }
+            }
         }
 
         // Up arrow
         else if (event.which === 38){
             var num = start - numRows;
             // Wrap Up to Down
-            if (num <= 0){
-                num = start + (numRows*(numRows-1));
+            if (num <= 0){ num = start + (numRows*(numRows-1)); }
+            // Skipping prefills
+            while (document.getElementById('c'+ num).classList.contains('prefill')){
+                num -= numRows;
+                if (num <= 0){ num = start + (numRows*(numRows-1)); }
             }
         }
 
         // Right arrow
         else if (event.which === 39){
             var num = start + 1;
+            // Bottom Right to Top Left Wrapping
+            if (num === 82) { num = 1; }
+            // Skipping prefills
+            while (document.getElementById('c'+ num).classList.contains('prefill')){
+                num += 1;
+                if (num === 82) { num = 1; }
+            }
         }
 
         // Down arrow
         else if (event.which === 40){
             var num = start + numRows;
             // Wrap Down to Up
-            if (num > 81){
-                num = start - (numRows*(numRows-1));
+            if (num > 81){ num = start - (numRows*(numRows-1)); }
+            // Skipping prefills
+            while (document.getElementById('c'+ num).classList.contains('prefill')){
+                num += numRows;
+                console.log(num)
+                if (num > 81){ num = start - (numRows*(numRows-1)); }
             }
         }
 
