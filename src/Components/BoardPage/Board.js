d6b0a4c91ebb13ce32d13f349c62500766a6a9b4
Jake Hracho <jhracho@nd.edu>
2022-01-25 20:50:03 -0500

    board sass done for now

--- src/Components/BoardPage/Board.js
+++ src/Components/BoardPage/Board.js
@@ -3,105 +3,87 @@ import React from "react";
 const Board = () =>{
     return(
         <div className="board">
-            <div className='cell-row'>
-                <input id = 'c1' className="cell" type="text" pattern="\d*" minLength="1" maxLength="1" required/>
-                <input id = 'c2' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c3' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c4' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c5' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c6' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c7' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c8' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c9' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c10' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c11' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c12' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c13' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c14' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c15' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c16' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c17' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c18' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c19' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c20' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c21' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c22' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c23' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c24' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c25' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c26' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c27' className="cell" type="text" inLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c28' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c29' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c30' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c31' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c32' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c33' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c34' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c34' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c36' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c37' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c38' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c39' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c40' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c41' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c42' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c43' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c44' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c45' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c46' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c47' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c48' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c49' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c50' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c51' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c52' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c53' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c54' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c55' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c56' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c57' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c58' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c59' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c60' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c61' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c62' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c63' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c64' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c65' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c66' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c67' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c68' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c69' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c70' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c71' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c72' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
-            <div className='cell-row'>
-                <input id = 'c73' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c74' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c75' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c76' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c77' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c78' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c79' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c80' className="cell" type="text" minLength="1" maxLength="1"/>
-                <input id = 'c81' className="cell" type="text" minLength="1" maxLength="1"/>
-            </div>
+            <input id = 'c1' className="cell" type="text" pattern="\d*" minLength="1" maxLength="1" required/>
+            <input id = 'c2' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c3' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c4' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c5' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c6' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c7' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c8' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c9' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c10' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c11' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c12' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c13' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c14' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c15' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c16' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c17' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c18' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c19' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c20' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c21' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c22' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c23' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c24' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c25' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c26' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c27' className="cell" type="text" inLength="1" maxLength="1"/>
+            <input id = 'c28' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c29' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c30' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c31' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c32' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c33' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c34' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c34' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c36' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c37' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c38' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c39' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c40' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c41' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c42' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c43' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c44' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c45' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c46' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c47' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c48' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c49' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c50' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c51' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c52' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c53' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c54' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c55' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c56' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c57' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c58' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c59' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c60' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c61' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c62' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c63' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c64' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c65' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c66' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c67' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c68' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c69' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c70' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c71' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c72' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c73' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c74' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c75' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c76' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c77' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c78' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c79' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c80' className="cell" type="text" minLength="1" maxLength="1"/>
+            <input id = 'c81' className="cell" type="text" minLength="1" maxLength="1"/>
         </div> 
     )  
 };
