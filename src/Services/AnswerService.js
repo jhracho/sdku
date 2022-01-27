a9b4cb06c66a43dd000b0daecb0d75cb817766ea
Jake Hracho <jhracho@nd.edu>
2022-01-27 16:36:59 -0500

    renamed from PuzzleService

---
+++ src/Services/AnswerService.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+// Grabs puzzle input for cross-referencing
+export const checkAnswer = (event) => {
+    // Setup
+    event.preventDefault();
+    var puzzle = "";
+
+    // Get input data
+    for (let i = 0; i < 81; i++){
+        var input = event.target[i].value;
+        if (input === ""){ 
+            alert('One or more squares is empty!');
+            return null;       
+        }
+        puzzle += event.target[i].value; 
+    }
+
+    // Compare puzzle to solution
+    console.log(puzzle);
+}
