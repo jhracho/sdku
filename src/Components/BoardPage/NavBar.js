2ba1012418e2047e18b80ff7282369e150643e7b
Jake Hracho <jhracho@nd.edu>
2022-01-27 16:36:53 -0500

    added new files

---
+++ src/Components/BoardPage/NavBar.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () =>{
+    return(
+        <div id='nav-area'>
+            <div id='logo'>
+                <h1>SDKU</h1>
+            </div>
+            <div id='hamburger-menu'>
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
+            </div>
+        </div>
+    );
+}
+
+export default NavBar;
---
+++ src/Components/BoardPage/Puzzle.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Board from './Board';
+import Submit from "./Submit";
+
+import { checkAnswer } from "../../Services/AnswerService.js";
+
+const Puzzle = () =>{
+    return (
+        <div id="board-container">
+            <form onSubmit={checkAnswer}>
+                <Board />
+                <Submit />
+            </form>
+        </div>
+    );
+}
+
+export default Puzzle;
